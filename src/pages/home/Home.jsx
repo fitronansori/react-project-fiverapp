0a9fcb32d9e8a6ab9285009b5ed04d3a8e52b343
Fitron Ansori <fitronansori7@gmail.com>
2023-04-17 14:48:16 +0800

    add img and make featured component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Home.scss";
+import Featured from "../../components/featured/Featured";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <Featured />
     </div>
   );
 };
